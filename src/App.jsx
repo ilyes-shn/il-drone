305afc1aa532a68cc5e010b9d9f852edfa9add65
ilyes-shn <infinityilyes@gmail.com>
2021-08-15 17:45:09 +0100

    drone

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Drone from './Drone';
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Drone />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Drone.jsx
@@ -0,0 +1,35 @@
+import React, { Suspense, useRef, useEffect } from "react";
+import { useAnimations, OrbitControls, Environment } from "@react-three/drei";
+import { Canvas, useLoader } from "@react-three/fiber";
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+
+const Models = () => {
+  const result = useLoader(GLTFLoader, '/flying/scene.gltf')
+  const drone = useRef()
+
+  const { actions } = useAnimations(result.animations, drone);
+
+  useEffect(() => {
+    const obj = Object.values(actions)[0]
+    obj.play()
+  }, [])
+  
+  return <primitive ref={drone} object={result.scene} />
+}
+
+const Drone = () => {
+  return (
+    <Suspense fallback={null}>
+      <Canvas camera={{ position: [0, 0.5, 7], fov: 50 }}>
+        <OrbitControls autoRotate/>
+        <ambientLight intensity={1} castShadow/>
+        <directionalLight intensity={1}/>
+        <Models />
+        <Environment files={'/quattro_canti_2k.hdr'} background/>
+      </Canvas>
+    </Suspense>
+  );
+}; 
+
+export default Drone;
