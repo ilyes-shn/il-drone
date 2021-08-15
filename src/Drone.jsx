aa14986fca00557999d485edf4ffabe4ebb842d8
ilyes-shn <infinityilyes@gmail.com>
2021-08-15 18:17:40 +0100

    Envirment

--- src/Drone.jsx
+++ src/Drone.jsx
@@ -1,35 +1,34 @@
 import React, { Suspense, useRef, useEffect } from "react";
 import { useAnimations, OrbitControls, Environment } from "@react-three/drei";
 import { Canvas, useLoader } from "@react-three/fiber";
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 const Models = () => {
-  const result = useLoader(GLTFLoader, '/flying/scene.gltf')
-  const drone = useRef()
+  const result = useLoader(GLTFLoader, "/flying/scene.gltf");
+  const drone = useRef();
 
   const { actions } = useAnimations(result.animations, drone);
 
   useEffect(() => {
-    const obj = Object.values(actions)[0]
-    obj.play()
-  })
-  
-  return <primitive ref={drone} object={result.scene} />
-}
+    const obj = Object.values(actions)[0];
+    obj.play();
+  });
+
+  return <primitive ref={drone} object={result.scene} />;
+};
 
 const Drone = () => {
   return (
     <Suspense fallback={null}>
-      <Canvas camera={{ position: [0, 0.5, 7], fov: 50 }}>
-        <OrbitControls autoRotate/>
-        <ambientLight intensity={1} castShadow/>
-        <directionalLight intensity={1}/>
+      <Canvas camera={{ position: [0, 0, 8], fov: 50 }}>
+        <OrbitControls autoRotate />
+        <ambientLight intensity={1} />
+        <directionalLight intensity={1} />
         <Models />
-        <Environment files={'/quattro_canti_2k.hdr'} background/>
+        <Environment files={"/old_hall_1k.hdr"} background />
       </Canvas>
     </Suspense>
   );
-}; 
+};
 
 export default Drone;
