e439337dc0c0b66dde96d891a4dad937dc5d83d2
ilyes-shn <infinityilyes@gmail.com>
2021-08-17 11:25:23 +0100

    gltfjsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Drone from './Drone';
+import Fly from './Fly';
 
 const App = () => {
   return (
     <div className='app'>
-      <Drone />
+      <Fly />
     </div>
   )
 }
